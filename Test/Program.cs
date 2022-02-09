c250db543e4e3ae2fbff9dd320cf74cd9fad1f37
Jonas Högele <jonas.h50@gso.schule.koeln>
2022-02-09 12:25:17 +0100

    Projektdateien hinzufügen.

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SchleifenA9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double K = 0, K_neu = 0, p = 0;
+            int jahre = 0;
+
+            Console.WriteLine("Geben Sie ihr Kapital an");
+            K = Convert.ToDouble(Console.ReadLine());
+            K_neu = K;
+
+            Console.WriteLine("Geben Sie ihren Zinssatz an");
+            p = Convert.ToDouble(Console.ReadLine());
+
+            while (K_neu < K * 2)
+            {
+                K_neu = K_neu + K_neu * (p / 100);
+                jahre++;
+            }
+
+            Console.WriteLine("Es dauert etwa {0} Jahre um das Kapital zu verdoppeln", jahre);
+        }
+    }
+}
---
+++ ZinsKAÜbung/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZinsKAÜbung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Jahr"; string b = "Guthaben"; string c = "Zinsen"; string d = "K-Zinsen"; string e = "Bilanz";
+
+            Console.WriteLine("Geben Sie Ihr Kapital an.");
+            double Kapital = Convert.ToDouble(Console.ReadLine()); Console.Clear();
+            Console.WriteLine("Nun geben Sie bitte Ihre Anlagedauer an.");
+            int Anlagedauer = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+            Console.WriteLine("Geben Sie nun Ihren Zinssatz ein.");
+            double Zinssatz = Convert.ToDouble(Console.ReadLine()); Console.Clear();
+
+            double KZinsen = 0;
+            double Bilanz = 0;
+
+            Console.WriteLine("{0,4} {1,10} {2,8} {3,10} {4,8}", a, b, c, d, e);
+
+            double neu_Kapital = Kapital;
+            double Zinsen = 0;
+
+
+            for (int i = 1; i <= 43; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.WriteLine();
+    
+            for (int i = 1; i <= Anlagedauer; i++)
+            {
+                Zinsen = neu_Kapital * (Zinssatz / 100);
+                KZinsen = KZinsen + Zinsen;
+                Bilanz = neu_Kapital + Zinsen;
+                Console.WriteLine("{0,4} {1,10:F2} {2,8:F2} {3,10:F2} {4,8:F2}", i, neu_Kapital, Zinsen, KZinsen, Bilanz);
+                neu_Kapital = neu_Kapital + Zinsen;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
