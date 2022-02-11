2f64309e203cd156cb93bf818386614e8d78e46d
Jonas Högele <jonas.h50@gso.schule.koeln>
2022-02-11 12:01:42 +0100

    Überischtlicher ; Weniger Zeilen

--- ZinsKAÜbung/Program.cs
+++ ZinsKAÜbung/Program.cs
@@ -19,32 +19,26 @@ namespace ZinsKAÜbung
             Console.WriteLine("Geben Sie nun Ihren Zinssatz ein.");
             double Zinssatz = Convert.ToDouble(Console.ReadLine()); Console.Clear();
 
-            double KZinsen = 0;
-            double Bilanz = 0;
+            double KZinsen = 0; double Bilanz = 0; double neu_Kapital = Kapital; double Zinsen = 0;
 
             Console.WriteLine("{0,4} {1,10} {2,8} {3,10} {4,8}", a, b, c, d, e);
-
-            double neu_Kapital = Kapital;
-            double Zinsen = 0;
-
-
             for (int i = 1; i <= 43; i++)
             {
                 Console.Write("-");
             }
-
             Console.WriteLine();
-    
             for (int i = 1; i <= Anlagedauer; i++)
             {
                 Zinsen = neu_Kapital * (Zinssatz / 100);
                 KZinsen = KZinsen + Zinsen;
                 Bilanz = neu_Kapital + Zinsen;
-                Console.WriteLine("{0,4} {1,10:F2} {2,8:F2} {3,10:F2} {4,8:F2}", i, neu_Kapital, Zinsen, KZinsen, Bilanz);
+                Console.WriteLine("{0,3} {1,9:F2}$ {2,7:F2}$ {3,9:F2}$ {4,9:F2}$", i, neu_Kapital, Zinsen, KZinsen, Bilanz);
                 neu_Kapital = neu_Kapital + Zinsen;
-
             }
-            Console.ReadKey();
+            for (int i = 1; i <= 43; i++)
+            {
+                Console.Write("-");
+            }   Console.ReadKey();
         }
     }
 }
