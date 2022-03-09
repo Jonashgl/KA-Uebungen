e05998cacec905755f94c94273ca0b554984df8c
Jonas Högele <jonas.h50@gso.schule.koeln>
2022-03-09 10:57:29 +0100

    ArrrayEinstieg Aufgabe gelöst und fertig

---
+++ ArrayEinstieg/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArrayEinstieg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            int[] Noten = new int[n];
+            int Noten2 = 0;
+            double Notensumme = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Geben Sie die {0} Note ein.", i+1);
+
+                Noten2 = Convert.ToInt32(Console.ReadLine());
+                Noten[i] = Noten2;
+                Notensumme = Notensumme + Noten2;
+            }
+            
+            Console.WriteLine($"Der Notendurschnitt beträgt {Notensumme / 20}\n");
+            Console.WriteLine("Die Noten werden nun untereinander ausgegeben");
+            foreach(int i in Noten) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
