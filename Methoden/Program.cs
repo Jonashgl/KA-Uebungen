365debcea241ef32c04670a35a6c96cf9fec696a
Jonas Högele <jonas.h50@gso.schule.koeln>
2022-03-23 12:54:16 +0100

    Try Catch hinzugefügt. Programm kann nun nicht mehr abstürzen!

--- Methoden/Program.cs
+++ Methoden/Program.cs
@@ -6,18 +6,33 @@ namespace ConsoleApp4
     {
         public static void Main(string[] args)
         {
-            int zeichenanzahl = 0;
-            int durchgang = 0;
+            bool Errorcode = false;
+            do
+            {
+                try
+                {
+                    Errorcode = false; 
+                    int zeichenanzahl = 0;
+                    int durchgang = 0;
 
-            Console.WriteLine("Geben Sie einen beliebigen Text ein.");
+                    Console.WriteLine("Geben Sie einen beliebigen Text ein.");
 
-            string text = Console.ReadLine();
-            Console.WriteLine("Geben Sie nun an, wie oft Ihr Durchgang durchlaufen werden soll");
-            durchgang = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
+                    string text = Console.ReadLine();
+                    Console.WriteLine("Geben Sie nun an, wie oft Ihr Durchgang durchlaufen werden soll");
+                    durchgang = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
 
-            zeichenanzahl = ausgabeTextMalX(text, durchgang);
-            Console.Write("\nAnzahl der Buchstaben: {0}", zeichenanzahl);
+                    zeichenanzahl = ausgabeTextMalX(text, durchgang);
+                    Console.Write("\nAnzahl der Buchstaben: {0}", zeichenanzahl);
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Falsche Eingaben, geben Sie nun erneut die richtige Eingabe ein.");
+                    Errorcode = true;
+                    Console.Clear();
+                }
+            } while (Errorcode == true);
         }
 
         static int ausgabeTextMalX(string i, int x)
