c18ac7ac26da66692a7700b71b289505e4f91d7e
Jonas Högele <jonas.h50@gso.schule.koeln>
2022-04-08 12:49:08 +0200

    Hotel Check.In

--- Hotel Check/Program.cs
+++ Hotel Check/Program.cs
@@ -14,7 +14,7 @@ namespace Hotel_Check
             double[] Rechnungsbetrag = { 82.46, 329.84, 989.52, 247 };
 
             string HauptAusw;
-            bool p_wiederholen = false;
+            bool wiederholen = false;
             do
             {
                 (int, int) cPosBM = Console.GetCursorPosition();
@@ -39,39 +39,70 @@ namespace Hotel_Check
                 Console.WriteLine("Geben Sie nun die Zimmernummer oder den Namen ein.\n");
                 Console.Write("Eingabe:");
                 HauptAusw = Console.ReadLine();
-
+                Console.Clear();
                 short HauptAusw_short;
-
                 bool flag = short.TryParse(HauptAusw, out HauptAusw_short);
 
                 if (flag == true)
                 {
-                    // Methode von Short einfügen
+                    var Daten = GetData(HauptAusw_short, Zimmernummer, Vorname, Nachname, Übernachtungen, Rechnungsbetrag);
+                    if(Daten.Item1 == true)
+                    {
+                        short zimmernummer_gast = Daten.Item2;
+                        string vorname_gast = Daten.Item3;
+                        string nachname_gast = Daten.Item4;
+                        short übernachtungen_gast = Daten.Item5;
+                        double rechnungsbetrag_gast = Daten.Item6;
+                        Console.WriteLine("Zimmernummer\tVorname\t\tNachname\tÜbernachtungen\tRechnungsbetrag");
+                        Console.WriteLine($"{zimmernummer_gast}\t\t{vorname_gast}\t\t{nachname_gast}\t\t{übernachtungen_gast}\t\t{rechnungsbetrag_gast}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kein Item gefunden!");
+                    }
                 }
                 else
                 {
-                    switch (HauptAusw)
+                    var daten = GetData(HauptAusw, Zimmernummer, Vorname, Nachname, Übernachtungen, Rechnungsbetrag);
+                    if (daten.Item1 == true) 
                     {
-
-
-                        case "exit":
-                            Environment.Exit(0);
-                            break;
-
-                        default:
-
-                            Console.WriteLine("Ungültige Eingabe");
-                            Console.ReadKey();
-
-                            (int, int) cPosAM = Console.GetCursorPosition();
-
-
-                            ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
-
-                            break;
+                        short zimmernummer_gast = daten.Item2;
+                        string vorname_gast = daten.Item3;
+                        string nachname_gast = daten.Item4;
+                        short übernachtungen_gast = daten.Item5;
+                        double rechnungsbetrag_gast = daten.Item6;
+                        Console.WriteLine("Zimmernummer\tVorname\t\tNachname\tÜbernachtungen\tRechnungsbetrag");
+                        Console.WriteLine($"{zimmernummer_gast}\t\t{vorname_gast}\t\t{nachname_gast}\t\t{übernachtungen_gast}\t\t{rechnungsbetrag_gast}");
                     }
                 }
-            } while (p_wiederholen == true);
+
+                Console.WriteLine("\nZum fortfahren eine beliebige Taste drücken.");
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.WriteLine("Möchten Sie das Programm wiederholen? Bei falscher Eingabe wird das Programm geschlossen.\n");
+                Console.WriteLine("Ja     - Programm wiederholen");
+                Console.WriteLine("Nein   - Programm schließen\n");
+                Console.WriteLine("Eingabe:");
+                string str_wiederholen;
+                str_wiederholen = Console.ReadLine();
+                switch (str_wiederholen)
+                {
+                    case "Ja":
+                        Console.Clear();
+                        wiederholen = true;
+                        break;
+
+                    case "Nein":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+
+                        Environment.Exit(0);
+                        break;
+                }
+            } while (wiederholen == true);
         }
 
 
@@ -108,20 +139,6 @@ namespace Hotel_Check
             }
 
             return (flag,zn[index],vn[index], nn[index], ue[index], rb[index]);
-        }
-
-        public static void ClearCurrentConsoleLine(int from, int to)
-        {
-
-            for (int i = to; i >= from; i++)
-            {
-                Console.SetCursorPosition(0, i);
-                Console.Write(new string(' ', Console.WindowWidth));
-            }
-
-            Console.SetCursorPosition(0, from);
-
-
         }
     }
 }
